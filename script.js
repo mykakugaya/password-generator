3910b8ed13f44758ac8a35f710cee47f5326416b
Myka Kugaya <mykakug129@gmail.com>
2020-04-22 16:16:37 -0700

    password generation js finished

--- script.js
+++ script.js
@@ -14,53 +14,66 @@ generateBtn.addEventListener("click", writePassword);
 
 // Generate the password for #password
 function generatePassword() {
-    var lowerchar = "abcdefghijklmnopqrstuvwxyz";
-    var upperchar = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    var numchar = "01234567890123456789";
-    var specialchar = "!'#$%&()*+,-./:;<=>?@[]^_`{|}~)";
+    // need " and \
+    var lowerChar = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+    var upperChar = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+    var numChar = ["0","1","2","3","4","5","6","7","8","9"];
+    var specialChar = ["!","'","#","$","%","&","(",")","*","+",",","-",".","/",":",";","<","=",">","?","@","[","]","^","_","`","{","|","}","~", '"'];
 
     var userChoices = [];
     var userPass = "";
 
+    // function that chooses randomly out of an array
+    function randomChar(array) {
+        var index = Math.floor(Math.random()*array.length);
+        return array[index];
+    }
+
   // Length? Range 8-128 characters
     var passLength = prompt("How many characters do you want in your password?");
 
-  // Lowercase?
-    var lowercase = confirm("Include lowercase letters in your password?");
-    if (lowercase===true) {
-        userChoices.push(lowerchar);
+    while (passLength<=8 || passLength>=128) {
+        alert("Password length must be between 8 and 128 characters.")
+        passLength = prompt("How many characters do you want in your password?");
     }
 
-  // Uppercase?
-    var uppercase = confirm("Include uppercase letters in your password?");
-    if (uppercase===true) {
-        userChoices.push(upperchar);
-    }
+    while (userChoices.length === 0) {
+        // Lowercase?
+        var lowercase = confirm("Include lowercase letters in your password?");
+        if (lowercase===true) {
+            userChoices.push(lowerChar);
+        }
 
-  // Numeric?
-    var numeric = confirm("Include numbers in your password?");
-    if (numeric===true) {
-        userChoices.push(numchar);
-    }
+    // Uppercase?
+        var uppercase = confirm("Include uppercase letters in your password?");
+        if (uppercase===true) {
+            userChoices.push(upperChar);
+        }
 
-  // Special characters?
-    var special = confirm("Include special characters in your password?");
-    if (special===true) {
-        userChoices.push(specialchar);
-    }
+    // Numeric?
+        var numeric = confirm("Include numbers in your password?");
+        if (numeric===true) {
+            userChoices.push(numChar);
+        }
 
-    if (userChoices.length == 0) {
-        alert("You must choose at least one character type!")
+    // Special characters?
+        var special = confirm("Include special characters in your password?");
+        if (special===true) {
+            userChoices.push(specialChar);
+        }
+
+        if (userChoices.length == 0) {
+            alert("You must choose at least one character type!")
+        }
     }
+  
 
 
   //Generate password of length passLength
-    for (i=0; i<userChoices.length; i++) {
+    for (i=0; i<passLength; i++) {
         var selectList = userChoices[(Math.floor(Math.random()*userChoices.length))];
-
-        for (i=0; i<selectList.length; i++) {
-            var passwordchar = selectList[Math.floor(Math.random()*selectList.length)]
-            userPass += passwordchar
-        }
+        userPass += randomChar(selectList)
     }
+
+    return userPass
 }
